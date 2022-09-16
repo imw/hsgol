1c3f59df4f25e93eb7038ea36a0e18ff783172e8
Nasreen </>
2022-09-16 16:02:27 +0100

    Print the board and start writing tick

--- gol.hs
+++ gol.hs
@@ -1,28 +1,54 @@
+import Data.Map.Strict as Map
+
 --type Cell = (int,int)
 boardState = [(1,1),(2,2),(3,3)]
 
-isAlive :: (Int,Int) -> [(Int,Int)] -> Bool
+--isAlive :: (Int,Int) -> [(Int,Int)] -> Bool
 isAlive cell [] = False
 
-isAlive cell boardState = 
-	if head boardState == cell
-		then True
-	else isAlive cell (tail boardState)
+isAlive cell boardState =
+  if head boardState == cell
+  then True
+  else isAlive cell (tail boardState)
 
-rowcells row = [(row,y) | y <- [1..10]]
+rowCells row = [(row,y) | y <- [1..10]]
 
 renderRow result [] boardState =
-	result
+  result
 
-renderRow result row boardState = 
-	if isAlive (head row) boardState
-	then renderRow (result ++ "X") (tail row) boardState
-	else renderRow (result ++ " ") (tail row) boardState
+renderRow result row boardState = -- renderRow "" (1,1) [(1,1),(2,2),(3,3)]
+  if isAlive (head row) boardState
+  then renderRow (result ++ "X") (tail row) boardState
+  else renderRow (result ++ " ") (tail row) boardState
 
-renderBoard = 
-	[1..10]
+rows = [1..10]
+renderBoard result [] boardState = result
+renderBoard result rows boardState = renderBoard (result++((renderRow "" (rowCells (head rows)) boardState) ++ "\n"))
+   (tail rows)
+   boardState
 
--- renderBoard =
+--tickN n boardState
+
+--tick boardState =
 
 
-main = print (renderRow "" (rowcells 1) boardState)
+getNeighborCount [] map = map
+getNeighborCount boardState map = case Map.lookup (head boardState) map of Nothing -> (getNeighborCount (tail boardState) (Map.insert (head boardState) 1 map))
+																					       													 (Just x) -> (getNeighborCount (tail boardState) (Map.adjust (+ 1) (head boardState) map))
+
+
+--getNeighborCount [] map = map
+--getNeighborCount y = case y of Nothing -> 1
+--															(Just x) -> 2
+
+-- iterate through the live rowCells
+-- update each live cells neighbors in hashmap
+
+
+main = putStrLn $ renderBoard "" [1..10] boardState
+
+
+
+--f (g x) = (f . g) x
+--f x = f . g . h x
+--f x = f ( g ( h x ) )
